9e2dc6c997449bf43d86aa0990cfd6ffe3890507
varunshuklaa <vanu.shuklaa@gmail.com>
2025-03-25 02:04:29 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import Home from './components/Home';
 import Paste from './components/Paste';
 import ViewPaste from './components/ViewPaste';
